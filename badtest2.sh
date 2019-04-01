0f213c5d531d54ffd1165f0e632ae9cceb4a289f
pi <84328802@qq.com>
2019-04-01 22:09:39 +0800

    how not to use the for command

---
+++ badtest2.sh
@@ -0,0 +1,12 @@
+#/*************************************************************************
+#	> File Name: badtest2.sh
+#	> Author: ch3cooh
+#	> Func:  
+#	> Created Time: 2019年04月01日 星期一 22时06分38秒
+# ************************************************************************/
+#!/bin/bash
+
+for test in Nevada New Hampshire New Mexico New York North Carolina
+do 
+	echo "Now going to $test"
+done
