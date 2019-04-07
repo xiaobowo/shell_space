14d019a4574f9d5df7be9a5a9e1c9c6912bc8009
pi <84328802@qq.com>
2019-04-07 21:21:25 +0800

    test command line option and value

---
+++ test16.sh
@@ -0,0 +1,26 @@
+#########################################################################
+# File Name: test16.sh
+# Author: ch3cooh
+# Func: 
+# Created Time: 2019年04月07日 星期日 21时01分32秒
+#########################################################################
+#!/bin/bash
+echo
+while [ -n "$1" ]
+do
+	case "$1" in
+		-a) echo Found the -a option;;
+		-b) echo Found the -b option;;
+		-c) echo Found the -c option;;
+		--) shift
+			break ;;
+		*) echo $1 is not an option;;
+	esac
+	shift
+done
+count=1
+for param in $@
+do
+	echo "Parament #$count:$param"
+	count=$[ $count + 1 ]
+done
